1756f2f5887a6ef498ec3d9b66d97d25586981bf
giancarloC <gcalle288@gmail.com>
2020-09-19 15:11:45 -0400

    added way to access dotenv file and grab bot token

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
+require('dotenv').config();
+const BOT_TOKEN = process.env.BOT_TOKEN
 const Discord = require('discord.js')
 const Bot = Discord.Client();
-
-
-
 
 Bot.once('ready', () => {
     console.log("Let's get quirky!! :D")
 });
-
-
-
-
-
 
 Bot.login("insert token");
