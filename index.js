f98aefd52e30798bb5f74a2f4ab0a7463ceb9de2
Marisa <mlsig95@gmail.com>
2020-09-18 20:32:25 -0400

    index file
    p much a hello world for the bot.

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js')
+const Bot = Discord.Client();
+
+
+
+
+Bot.once('ready', () => {
+    console.log("Let's get quirky!! :D")
+});
+
+
+
+
+
+
+Bot.login("insert token");
